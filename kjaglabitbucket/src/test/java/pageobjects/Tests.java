a220670e89c0d7429a7827250fa77bbfda0be43a
Your Name <you@example.com>
2018-06-20 21:22:39 +0200

    badges baste tests

---
+++ kjaglabitbucket/src/test/java/pageobjects/Tests.java
@@ -0,0 +1,48 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by karol on 31.10.17.
+ */
+public class Tests {
+    private WebDriver driver;
+    private Login login;
+    private final static String URL = "https://book.jetblue.com/B6.auth/login?&service=https://badges.jetblue.com";
+
+    @BeforeTest
+    public void setup() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(21310, TimeUnit.SECONDS); //nnn
+        driver.get(URL);
+    }
+
+    @Test
+    public void login(){
+        login = new Login(driver);
+        Home home = login.loginToHome("jagla.karol@gmail.com","Karol2017jb");
+        home.checkSuccessfullLogin();
+        Logout logout = home.logout();
+        Assert.assertTrue(logout.we);
+
+    }
+
+    @Test
+    public void t1(){
+        Assert.assertEquals(1,1);
+    }
+
+    @AfterTest
+    public void quit() {
+        driver.quit();
+    }
+
+
+}
